49322d03412070a3fe935414cdfdca5e4419bf7b
Garrit Wilson <wilsongarrit@gmail.com>
2022-01-25 18:20:01 +0100

    first commit

---
+++ passenger_counter_app/index.js
@@ -0,0 +1,16 @@
+// document.getElementById("count-el").innerText = 4
+
+let count = 5
+
+let bonusPoints = 50
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+
