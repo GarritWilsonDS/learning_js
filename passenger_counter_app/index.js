e4b0f47042d8ec41ad8dcb813525183557fc4459
Garrit Wilson <wilsongarrit@gmail.com>
2022-01-26 12:13:23 +0100

    made progress on person tracking app

--- passenger_counter_app/index.js
+++ passenger_counter_app/index.js
@@ -1,16 +1,53 @@
-// document.getElementById("count-el").innerText = 4
-
-let count = 5
-
-let bonusPoints = 50
-
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
 
 
+
+// initialize count at 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked (log it out)
+// change the count-el in the html to reflect new count
+
+let count = 0
+
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("saved-el");
+
+function increment(){
+    console.log("increased by 1");
+    count++;
+    countEl.innerText = count;
+    console.log(count);
+}
+
+function decrease(){
+    console.log("decreased by 1");
+    count--;
+    countEl.innerText = count;
+    console.log(count);
+}
+
+function save(){
+    console.log("saved count");
+    saveEl.innerText = count;
+    console.log("count saved");
+    count = 0;
+    countEl.innerText = count;
+    console.log("reset count");
+}
+
+
+
+//// practicing functions
+
+let lapsCompleted = 0;
+
+function incrementByOne(x) {
+    x++;
+    return x
+}
+
+for (let i = 0; i<7; i++){
+    lapsCompleted = incrementByOne(lapsCompleted);
+}
+
+
+// console.log(lapsCompleted)
